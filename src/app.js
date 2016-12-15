f944a0108953a9f59a5df2f0921d7609001fd0c7
troy.tao <troy.tao@rangle.io>
2016-12-15 10:41:08 -0500

    Enable throng in production

--- src/app.js
+++ src/app.js
@@ -10,31 +10,38 @@ import { rootRouter } from './routes/root.routes';
 import { healthCheckRouter } from './routes/health-check/health-check.routes';
 import { demoRouter } from './routes/demo/demo.routes';
 
-// Entry point for all modules.
-const api = koaRouter()
-  .get('/', rootRouter.routes())
-  .use('/health', healthCheckRouter.routes())
-  .use('/demo', demoRouter.routes());
+function setUpServer(app) {
+  // Entry point for all modules.
+  const api = koaRouter()
+    .get('/', rootRouter.routes())
+    .use('/health', healthCheckRouter.routes())
+    .use('/demo', demoRouter.routes());
+
+  /* istanbul ignore if */
+  if (REQUEST_LOGS) {
+    app.use(require('koa-morgan')('combined'));
+  }
+
+  app
+    .use(helmet())
+    .use(koaConvert(bodyParser()))
+    .use(errorResponder)
+    .use(api.routes())
+    .use(api.allowedMethods());
+  return app;
+}
+
+function startFunction() {
+  const app = setUpServer(new Koa());
+  const PORT = process.env.PORT || 3000;
+  /* istanbul ignore if */
+  if (require.main === module) {
+    winston.info(`Starting server on port ${PORT}`);
+    app.listen(PORT);
+  }
+}
+const throng = require('throng');
+throng(startFunction);
 
 // Top level server configuration.
-export const app = new Koa();
-
-/* istanbul ignore if */
-if (REQUEST_LOGS) {
-  app.use(require('koa-morgan')('combined'));
-}
-
-app
-  .use(helmet())
-  .use(koaConvert(bodyParser()))
-  .use(errorResponder)
-  .use(api.routes())
-  .use(api.allowedMethods());
-
-const PORT = process.env.PORT || 3000;
-
-/* istanbul ignore if */
-if (require.main === module) {
-  winston.info(`Starting server on port ${PORT}`);
-  app.listen(PORT);
-}
+export const app = setUpServer(new Koa());
